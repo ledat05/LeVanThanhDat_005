f1d128b7734967f56b45838d2ed67bcfbac463ba
ADMIN <ADMIN@PC333>
2024-01-17 10:13:12 +0700

    Add project files.

---
+++ BT02/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT02
+{
+    class Program
+    {
+        class ChuNhat
+        {
+            private double rong;
+            private double dai;
+            public ChuNhat()
+            {
+                rong = 5;
+                dai = 6;
+                    }
+            public ChuNhat(double rong, double dai)
+            {
+                this.rong = rong;
+                this.dai = dai;
+            }
+            public double Rong
+            {
+                set { this.rong = value; }
+                get { return rong; }
+            }
+            public double Dai
+            {
+                set { this.dai = value; }
+                get { return dai; }
+            }
+            public virtual double tinhChuVi()
+            {
+                return dai + rong * 2;
+            }
+            public virtual double tinhDienTich()
+            {
+                return dai * rong;
+            }
+            public virtual void Xuat()
+            {
+                Console.WriteLine("Xuat Chu Vi :{0}", tinhChuVi());
+                Console.WriteLine("Xuat Dien Tich :{0}", tinhDienTich());
+            }
+            class Vuong : ChuNhat
+            {
+                private double canh;
+                public Vuong()
+                {
+                    canh = 4;
+                }
+                public Vuong(double canh)
+                {
+                    this.canh = canh;
+                }
+                public double Canh
+                {
+                    set { this.canh = value; }
+                    get { return canh; }
+                }
+                public override double tinhChuVi()
+                {
+                    return canh * 4;
+                }
+                public override double tinhDienTich()
+                {
+                    return canh * canh;
+                }
+                public override void Xuat()
+                {
+                    base.Xuat();
+                }
+                static void Main(string[] args)
+                {
+                    ChuNhat cn = new ChuNhat();
+                    cn.tinhChuVi();
+                    cn.tinhDienTich();
+                    cn.Xuat();
+
+                    Vuong vu = new Vuong();
+                    vu.tinhChuVi();
+                    vu.tinhDienTich();
+                    vu.Xuat();
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ bt01/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt01
+{
+    class NhanVien
+    {
+        private string Hoten, BoPhan;
+        private double SoNgayCong, SoNgayLamThem, XepLoai;
+
+        public NhanVien()
+        { }
+        public NhanVien(string hoten, double songaycong, double songaylamthem, string bophan, double xeploai)
+        {
+            this.Hoten = hoten;
+            this.SoNgayCong = songaycong;
+            this.SoNgayLamThem = songaylamthem;
+            this.BoPhan = bophan;
+            this.XepLoai = xeploai;
+        }
+        public string Hotem
+        {
+            set { this.Hoten = value; }
+            get { return Hoten; }
+        }
+        public double songaycong
+        {
+            set { this.SoNgayCong = value; }
+            get { return SoNgayCong; }
+        }
+        public double songaylamthem
+        {
+            set { this.SoNgayLamThem = value; }
+            get { return SoNgayLamThem; }
+        }
+        public string bophan
+        {
+            set { this.BoPhan = value; }
+            get { return BoPhan; }
+
+        }
+        public double xeploai
+        {
+            set { this.XepLoai = value; }
+            get { return XepLoai; }
+        }
+        public double TinhLuong()
+        {
+            double luongNgay = 100000; 
+            double phanTramTang = 0;
+
+            if (BoPhan == "TrucTiep")
+            {
+                //300% = 0,3
+                phanTramTang = 0.3;
+            }
+            else if (BoPhan == "GianTiep")
+            {
+                //200%=0,2
+                phanTramTang = 0.2;
+            }
+            // Tinh lương
+            double luong = (SoNgayCong + SoNgayLamThem * (1 + phanTramTang)) * luongNgay;
+
+            return luong;
+        }
+        public double TinhThuNhap()
+        {
+            return 0;
+        }
+        public void InThongTin()
+        {
+
+        }
+       
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NhanVien nv = new NhanVien();
+                nv.TinhLuong();
+            Console.ReadLine();
+        }
+    }
+}
